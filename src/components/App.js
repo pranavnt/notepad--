bb62b80ce181de44eceeb86b2b18120cbde90136
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-16 02:30:42 -0800

    kinda works rn

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
 import React from "react";
-
 import "./App.css";
 
-function App() {
-  return (
-    <div style={{ padding: "1vh" }}>
-      <h1>Notepad--</h1>
-      <textarea id="text" />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      body: "Welcome to Notepad-- :D",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ body: event.target.value });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Notepad--</h1>
+
+        <textarea
+          id="text"
+          value={this.state.body}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
